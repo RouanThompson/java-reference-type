171e26d7472851d7afa4915979911112f0551086
rouanthompson <rouan.thompson@gmail.com>
2021-04-02 16:19:14 -0400

    Initial commit

---
+++ src/com/FirstJavaSteps/Main.java
@@ -0,0 +1,13 @@
+package com.FirstJavaSteps;
+
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    byte age = 30;
+        Date now = new Date();
+        // now is variable that is an instance of the class Date
+        System.out.println(now);
+    }
+}
